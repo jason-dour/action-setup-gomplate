487b33ac64f6fd9ef5a74026f519358e11cafbcd
Jason A. Dour <10261648+jason-dour@users.noreply.github.com>
2022-05-30 15:27:30 -0400

    Update index.js

--- index.js
+++ index.js
@@ -1,10 +1,10 @@
-import { chmodSync } from 'fs';
-import { join } from 'path';
-import { getInput, addPath, setFailed } from '@actions/core';
-import { downloadTool } from '@actions/tool-cache';
-import { GitHub } from '@actions/github';
+const fs = require('fs');
+const path = require('path');
+const core = require('@actions/core');
+const tc = require('@actions/tool-cache');
+const github = require('@actions/github');
 
-const octokit = new GitHub(process.env.GITHUB_TOKEN);
+const octokit = new github.GitHub(process.env.GITHUB_TOKEN);
 
 async function getRelease(version) {
     if (version === 'latest') {
@@ -26,29 +26,29 @@ async function getDownloadObject(version) {
   const asset = release.data.assets.find(asset => asset.name.endsWith('_linux-amd64'));
   const url = asset.browser_download_url;
   console.log("download url: " + url);
-  const binPath = join("gomplate_linux-amd64");
+  const binPath = path.join("gomplate_linux-amd64");
   return { url, binPath };
 }
 
 async function setup() {
   try {
     // Get version of tool to be installed
-    const version = getInput('gomplate-version');
+    const version = core.getInput('gomplate-version');
 
     // Download the specific version of the tool.
     const download = await getDownloadObject(version);
-    const pathToCLI = await downloadTool(download.url,process.env.RUNNER_TEMP+"/gomplate");
-    chmodSync(pathToCLI, 0o755); // make the binary executable
+    const pathToCLI = await tc.downloadTool(download.url,process.env.RUNNER_TEMP+"/gomplate");
+    fs.chmodSync(pathToCLI, 0o755); // make the binary executable
     console.log("pathToCLI=" + pathToCLI);
 
     // Expose the tool by adding it to the PATH
-    addPath(process.env.RUNNER_TEMP);
+    core.addPath(process.env.RUNNER_TEMP);
   } catch (e) {
-    setFailed(e);
+    core.setFailed(e);
   }
 }
 
-export default setup
+module.exports = setup
 
 if (require.main === module) {
   setup();
