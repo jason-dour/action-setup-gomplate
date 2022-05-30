8fef23e304342d8901045fba093093084f99751c
Jason A. Dour <10261648+jason-dour@users.noreply.github.com>
2022-05-30 13:23:12 -0400

    Removed untar/unzip.

--- index.js
+++ index.js
@@ -44,12 +44,11 @@ async function setup() {
 
     // Download the specific version of the tool, e.g. as a tarball/zipball
     const download = await getDownloadObject(version);
-    console.log("download=" + download);
-    const pathToTarball = await tc.downloadTool(download.url);
+    const pathToCLI = await tc.downloadTool(download.url);
 
     // Extract the tarball/zipball onto host runner
-    const extract = download.url.endsWith('.zip') ? tc.extractZip : tc.extractTar;
-    const pathToCLI = await extract(pathToTarball);
+    // const extract = download.url.endsWith('.zip') ? tc.extractZip : tc.extractTar;
+    // const pathToCLI = await extract(pathToTarball);
 
     // Expose the tool by adding it to the PATH
     core.addPath(path.join(pathToCLI, download.binPath));
